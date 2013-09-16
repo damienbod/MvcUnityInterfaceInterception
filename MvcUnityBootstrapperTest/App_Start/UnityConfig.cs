3d8139cefc46e8ef6d99b8c08266d2b574e11b0e
damienbod <damien_bod@hotmail.com>
2013-09-16 13:16:47 +0200

    Init

---
+++ MvcUnityBootstrapperTest/App_Start/UnityConfig.cs
@@ -0,0 +1,70 @@
+using System;
+using Microsoft.Practices.Unity;
+using Microsoft.Practices.Unity.Configuration;
+using MvcUnityBootstrapperTest.Business;
+using MvcUnityBootstrapperTest.UnityExtensions;
+using System.Collections.Generic;
+using Microsoft.Practices.Unity.InterceptionExtension;
+
+namespace MvcUnityBootstrapperTest.App_Start
+{
+    /// <summary>
+    /// Specifies the Unity configuration for the main container.
+    /// </summary>
+    public class UnityConfig
+    {
+        #region Unity Container
+        private static Lazy<IUnityContainer> container = new Lazy<IUnityContainer>(() =>
+        {
+            var container = new UnityContainer();
+            RegisterTypes(container);
+            return container;
+        });
+
+        public static IUnityContainer GetConfiguredContainer()
+        {
+            return container.Value;
+        }
+        #endregion
+
+        public static void RegisterTypes(IUnityContainer container)
+        {
+            container.AddNewExtension<Interception>();
+
+            container.RegisterTypes(UnityHelpers.GetTypesWithCustomAttribute<UnityIoCPerRequestLifetimeAttribute>(AppDomain.CurrentDomain.GetAssemblies()),
+                                    WithMappings.FromMatchingInterface,
+                                    WithName.Default,
+                                    PerRequest
+                                )
+                     .RegisterTypes(UnityHelpers.GetTypesWithCustomAttribute<UnityIoCTransientLifetimeAttribute>(AppDomain.CurrentDomain.GetAssemblies()),
+                                    WithMappings.FromMatchingInterface,
+                                    WithName.Default,
+                                    WithLifetime.Transient
+                                );
+    
+            DiagnosisBehaviour d = new DiagnosisBehaviour();
+
+            container.AddNewExtension<Interception>();
+
+            container.RegisterType<IBusinessClass2>(
+                new InterceptionBehavior(d),
+                new Interceptor(new InterfaceInterceptor()));
+
+            container.RegisterType<IBusinessClass>(
+                new InterceptionBehavior(d),
+                new Interceptor(new InterfaceInterceptor()));
+
+            // This method checks and for classes from Loaded Assemblies and creates a per request lifetime object for classes with the custom attribute 
+            //container.RegisterTypes(AllClasses.FromLoadedAssemblies(),
+            //                        UnityHelpers.FromAllInterfacesWith_PerRequestLifetimeAttribute,
+            //                        WithName.Default,
+            //                        PerRequest
+            //                    )
+            //         .RegisterType<IUnitOfWorkExample, UnitOfWorkExampleTest>(new TransientLifetimeManager());
+ 
+        }
+
+        public static Func<System.Type, Microsoft.Practices.Unity.LifetimeManager> PerRequest = (x) => new PerRequestLifetimeManager();
+    }
+}
+
---
+++ MvcUnityBootstrapperTest/Business/BusinessClass2.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using MvcUnityBootstrapperTest.Logging;
+using MvcUnityBootstrapperTest.UnityExtensions;
+
+namespace MvcUnityBootstrapperTest.Business
+{
+    [UnityIoCPerRequestLifetimeAttribute]
+    public class BusinessClass2 : IBusinessClass2
+    {
+        private readonly IUnitOfWorkExample _unitOfWorkExample;
+
+        public BusinessClass2(IUnitOfWorkExample unitOfWorkExample)
+        {
+            _unitOfWorkExample = unitOfWorkExample;
+            UnityEventLogger.Log.CreateUnityMessage("BusinessClass2");
+        }
+
+        private bool _disposed = false;
+
+        public string Hello()
+        {
+            return "Hello from Business class2";
+        }
+
+        public string SayHello(string hello)
+        {
+            //throw new ArgumentNullException(hello);
+            return "SayHello Business class2 " + hello;
+        }
+
+        public void Dispose()
+        {
+            _unitOfWorkExample.Dispose();
+            UnityEventLogger.Log.DisposeUnityMessage("BusinessClass2");
+            if (!_disposed)
+            {
+                _disposed = true;
+            }
+        }
+    }
+}
